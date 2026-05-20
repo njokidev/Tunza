b968f09b9cdd76d2a686a2b898dfc8183f53e8fd
njoki111 <njokimitchelle111@gmail.com>
2026-05-20 23:50:09 +0300

    remove redundant folder

--- frontend/app/native-indent.tsx
+++
@@ -1,3 +0,0 @@
-export default function NativeIndent() {
-  return null
-}
--- frontend/app/test.jsx
+++
@@ -1,9 +0,0 @@
-import { supabase } from "../lib/supabaseClient";
-
-const testConnection = async () => {
-  const { data, error } = await supabase.auth.getSession();
-  console.log("SESSION:", data);
-  console.log("ERROR:", error);
-};
-
-testConnection();
